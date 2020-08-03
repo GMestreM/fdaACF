8ea1f833892a6f35c96616947a5d13d014e37539
GMestreM <guillermo.mestre@comillas.edu>
2020-08-03 20:41:48 +0200

    Add functions to estimate the partial autocorrelation function of a FTS

--- R/functional_autocorrelation.R
+++ R/functional_autocorrelation.R
@@ -1,5 +1,4 @@
 obtain_FACF <- function(Y,v,nlags,ci=0.95,estimation = "MC",figure = TRUE,...){
-
   #' Obtain the autocorrelation function for a given functional time series.
   #'
   #' @description Estimate the lagged autocorrelation function for a given
@@ -45,7 +44,19 @@ obtain_FACF <- function(Y,v,nlags,ci=0.95,estimation = "MC",figure = TRUE,...){
   #'     \item \code{rho}: Autocorrelation values for
   #'     each lag of the functional time series.
   #' }
-  #' @references \url{https://www.sciencedirect.com/science/article/pii/S0047259X17303512}
+  #' @references
+  #' Mestre, G., Portela, J., Muñoz-San Roque, A., Alonso, E. (2020).
+  #' \emph{Forecasting hourly supply curves in the Italian Day-Ahead 
+  #' electricity market with a double-seasonal SARMAHX model.}
+  #' International Journal of Electrical Power & Energy Systems, 
+  #' 121, 1--13. \url{https://www.sciencedirect.com/science/article/pii/S0142061519337135}
+  #' 
+  #' Kokoszka, P., Rice, G., Shang, H.L. (2017).
+  #' \emph{Inference for the autocovariance of a functional
+  #'  time series under conditional heteroscedasticity}
+  #' Journal of Multivariate Analysis, 
+  #' 162, 32--50. \url{https://www.sciencedirect.com/science/article/pii/S0142061519337135}
+  #' 
   #' @examples
   #' # Example 1
   #' 
@@ -60,7 +71,7 @@ obtain_FACF <- function(Y,v,nlags,ci=0.95,estimation = "MC",figure = TRUE,...){
   #' 
   #' data(elec_prices)
   #' v <- seq(from = 1, to = 24)
-  #' nlags <- 50
+  #' nlags <- 30
   #' obtain_FACF(Y = as.matrix(elec_prices), 
   #' v = v,
   #' nlags = nlags,
@@ -104,6 +115,212 @@ obtain_FACF <- function(Y,v,nlags,ci=0.95,estimation = "MC",figure = TRUE,...){
   output <- list()
   output$Blueline <- Blueline
   output$rho      <- rho
+  return(output)
+}
+
+obtain_FPACF <- function(Y,v,nlags,n_harm,ci=0.95,estimation = "MC",figure = TRUE,...){
+  #' Obtain the partial autocorrelation function for a given FTS.
+  #'
+  #' @description Estimate the partial autocorrelation
+  #' function for a given functional time series and its
+  #' distribution under the hypothesis of strong functional
+  #' white noise.
+  #'
+  #' @param Y Matrix containing the discretized values
+  #' of the functional time series. The dimension of the
+  #' matrix is \eqn{(n x m)}, where \eqn{n} is the
+  #' number of curves and \eqn{m} is the number of points
+  #' observed in each curve.
+  #' @param v Discretization points of the curves, by default
+  #' \code{seq(from = 0, to = 1, length.out = 100)}.
+  #' @param nlags Number of lagged covariance operators
+  #' of the functional time series that will be used
+  #' to estimate the partial autocorrelation function.
+  #' @param n_harm Number of principal components
+  #' that will be used to fit the ARH(p) models.
+  #' @param ci A value between 0 and 1 that indicates
+  #' the confidence interval for the i.i.d. bounds
+  #' of the partial autocorrelation function. By default
+  #' \code{ci = 0.95}.
+  #' @param estimation Character specifying the
+  #' method to be used when estimating the distribution
+  #' under the hypothesis of functional white noise.
+  #' Accepted values are:
+  #' \itemize{
+  #'    \item "MC": Monte-Carlo estimation.
+  #'    \item "Imhof": Estimation using Imhof's method.
+  #' }
+  #' By default, \code{estimation = "MC"}.
+  #' @param figure Logical. If \code{TRUE}, plots the
+  #' estimated partial autocorrelation function with the
+  #' specified i.i.d. bound.
+  #' @param ... Further arguments passed to the \code{plot_FACF}
+  #' function.
+  #' @return Return a list with:
+  #' \itemize{
+  #'     \item \code{Blueline}: The upper prediction
+  #'     bound for the i.i.d. distribution.
+  #'     \item \code{rho}: Partial autocorrelation
+  #'     coefficients for
+  #'     each lag of the functional time series.
+  #' }
+  #' 
+  #' 
+  #' @examples
+  #' # Example 1
+  #' 
+  #' N <- 100
+  #' v <- seq(from = 0, to = 1, length.out = 5)
+  #' sig <- 2
+  #' Y <- simulate_iid_brownian_bridge(N, v, sig)
+  #' obtain_FPACF(Y,v,20, n_harm = 2)
+  #' 
+  #' \donttest{
+  #' # Example 2
+  #' 
+  #' data(elec_prices)
+  #' v <- seq(from = 1, to = 24)
+  #' nlags <- 30
+  #' obtain_FPACF(Y = as.matrix(elec_prices), 
+  #' v = v,
+  #' nlags = nlags,
+  #' n_harm = 5, 
+  #' ci = 0.95,
+  #' figure = TRUE)
+  #' }
+  #' @export obtain_FPACF
+  
+  y <- Y
+  dv <- length(v)
+  dt <- nrow(y)
+  
+  # Initialize FPACF vector
+  FPACF <- rep(NA, nlags)
+  
+  FACF <- fdaACF::obtain_FACF(Y = y,
+                              v = v,
+                              nlags = 1,
+                              ci = ci,
+                              figure = F)
+  
+  Blueline <- FACF$Blueline
+  FPACF[1] <- FACF$rho[1]
+  
+  vector_PACF <- FPACF
+  
+  # Start loop for fitting ARH(p-1)
+  for(pp in 2:nlags){
+    lag_PACF <- pp
+    
+    # 1 - Fit ARH(1) to the series
+    Yest_ARIMA <- fit_ARHp_FPCA(y = y, v = v, p = lag_PACF-1, n_harm = n_harm)$y_est
+    if(F){
+      kkk <- 30
+      plot(v,y[kkk,],type = "b")
+      lines(v,Yest_ARIMA[kkk,],col = "red")
+    }
+    
+    # 2 - Fit ARH(1) to the REVERSED series
+    y_rev <- y[seq(from = nrow(y), to = 1, by = -1),]
+    
+    # y_rev[1,] == y[dt,]
+    
+    Yest_ARIMA_REV <- fit_ARHp_FPCA(y = y_rev, v = v, p = lag_PACF-1, n_harm = n_harm)$y_est
+    
+    # 3 - Estimate covariance surface for PACF 
+    Yest_1 <- Yest_ARIMA;
+    Yest_2 <- Yest_ARIMA_REV[seq(from = nrow(y), to = 1, by = -1),]
+    
+    res_filt_1 = y - Yest_1;
+    res_filt_2 = y - Yest_2;
+    
+    # Cross-covariance surface
+    sup_cov = matrix(0,dv,dv);
+    ini_serie = max(which(is.na(Yest_1[,1])))+2 #max(find(isnan(Yest_1(:,1))))+1;
+    fin_serie = min(which(is.na(Yest_2[,1])))-2 #min(find(isnan(Yest_2(:,1))))-1;
+    
+    count <- 0;
+    for(jj in ini_serie:fin_serie){
+      epsilon_1 <- as.matrix(res_filt_1[jj,])
+      epsilon_2 <- as.matrix(res_filt_2[jj-lag_PACF,])
+      
+      sup_cov <- sup_cov + (epsilon_1 %*% t(epsilon_2))
+      
+      count <- count + 1;
+    }
+    
+    sup_cov = (1/count)*sup_cov
+    
+    if(F){
+      persp(v,v,sup_cov,theta = 330, phi = 20, 
+            main = paste0("Covariance surface PACF lag ",lag_PACF),
+            ticktype = "detailed")
+    }
+    
+    # L2 norm of covariance surface
+    if(F){
+      sqrt(fdaACF::obtain_suface_L2_norm(v, list(Lag0 = sup_cov)))
+    }
+    
+    
+    # Estimate traces
+    var_1 <- matrix(0,dv,dv);
+    count <- 0;
+    for (jj in ini_serie:dt){
+      epsilon_1 = as.matrix(res_filt_1[jj,]);
+      # epsilon_2 = as.matrix(res_filt_1[jj,]);
+      
+      var_1 <- var_1 + (epsilon_1 %*% t(epsilon_1))
+      
+      count <- count + 1
+      
+    }
+    var_1 = (1/count)*var_1
+    
+    
+    
+    if(F){
+      persp(v,v,var_1,theta = 330, phi = 20, ticktype = "detailed", main = "var 1") 
+    }
+    traza_1 <- pracma::trapz(v,diag(var_1));
+    
+    var_2 <- matrix(0,dv,dv);
+    count <- 0;
+    for (jj in 1:fin_serie){
+      epsilon_1 <- as.matrix(res_filt_2[jj,]);
+      epsilon_2 <- as.matrix(res_filt_2[jj,]);
+      
+      var_2 <- var_2 + (epsilon_1 %*% t(epsilon_2))
+      
+      count <- count + 1;
+      
+      
+    }
+    var_2 = (1/count)*var_2
+    if(F){
+      persp(v,v,var_2,theta = 330, phi = 20, ticktype = "detailed", main = "var 2") 
+    }
+    
+    traza_2 = pracma::trapz(v,diag(var_2));
+    
+    
+    sup_corr = sup_cov/(sqrt(traza_1)*sqrt(traza_2));
+    
+    # L2 norm of cross-correlation surface
+    if(F){
+      sqrt(fdaACF::obtain_suface_L2_norm(v, list(Lag0 = sup_corr)))    
+    }
+    
+    vector_PACF[lag_PACF] <- sqrt(fdaACF::obtain_suface_L2_norm(v, list(Lag0 = sup_corr)))
+  }
+  
+  if(figure){
+    plot_FACF(vector_PACF,Blueline,ci,...)
+  }
+  
+  # Prepare output
+  output <- list(Blueline = Blueline,
+                 rho = vector_PACF)
   return(output)
 }
 
@@ -303,136 +520,4 @@ obtain_autocorrelation <- function(Y,v = seq(from = 0, to = 1, length.out = ncol
 }
 
 
-obtain_covariance <- function(Y,X,nlags = 0){
-  #' Estimate the covariance function of two FTS
-  #'
-  #' @description Obtain the empirical covariance function 
-  #' of the functional time
-  #' series \eqn{Y_{t}} and \eqn{X_{t}}. Given 
-  #' \eqn{Y_{1},...,Y_{T}} and \eqn{X_{1},...,X_{T}} two
-  #' functional time
-  #' series, the sample autocovariance functions
-  #' \eqn{\hat{\Sigma}_{Y,X}(u,v)} are given by:
-  #' \deqn{\hat{\Sigma}_{Y,X}(u,v) =  \frac{1}{T} \sum_{i=1}^{T-h}(Y_{i}(u) - \overline{Y}_{T}(u))(X_{i}(v) - \overline{X}_{T}(v))}
-  #' where
-  #' \eqn{ \overline{Y}_{T}(u) = \frac{1}{T} \sum_{i = 1}^{T} Y_{i}(t)}
-  #' denotes the sample mean function for functional time series \eqn{Y_{t}}.
-  #'
-  #' @param Y Matrix containing the discretized values
-  #' of the first functional time series. The dimension of the
-  #' matrix is \eqn{(n x m)}, where \eqn{n} is the
-  #' number of curves and \eqn{m} is the number of points
-  #' observed in each curve.
-  #' @param X Matrix containing the discretized values
-  #' of the second functional time series. The dimension of the
-  #' matrix is \eqn{(n x q)}, where \eqn{n} is the
-  #' number of curves and \eqn{q} is the number of points
-  #' observed in each curve.
-  #' @param nlags Number of lagged covariance operators
-  #' of the functional time series that will be estimated.
-  #' By default \code{nlags = 0}.
-  #' @return Return a list that contains the lagged covariance
-  #' functions of time series \eqn{Y} and \eqn{X}. Each 
-  #' element of the list contains a matrix with the 
-  #' discretized values of the lagged covariance function
-  #' estimated from the data. The covariance function is 
-  #' given by a \eqn{(m x q)} matrix, where \eqn{m} is the
-  #' number of points observed in curve \eqn{Y_{t}} and
-  #' \eqn{q} is the number of points observed in curve 
-  #' \eqn{X_{t}}.
-  #' @examples
-  #' # Example 1
-  #' 
-  #' N <- 100
-  #' v <- seq(from = 0, to = 1, length.out = 10)
-  #' sig <- 2
-  #' Y <- simulate_iid_brownian_bridge(N, v, sig)
-  #' X <- simulate_iid_brownian_bridge(N, v, 0.5)
-  #' nlags <- 1
-  #' cov <- obtain_covariance(Y = Y,
-  #'                          X = X,
-  #'                          nlags = nlags)
-  #' image(x = v, y = v, z = cov$Lag0)
-  #' 
-  #' \donttest{
-  #' # Example 2
-  #'
-  #' N <- 500
-  #' v <- seq(from = 0, to = 1, length.out = 50)
-  #' sig <- 2
-  #' Y <- simulate_iid_brownian_bridge(N, v, sig)
-  #' Y1 <- Y[1:N-1,]
-  #' Y2 <- Y[2:N,]
-  #' nlags <- 10
-  #' cov <- obtain_covariance(Y = Y1, X = Y2, nlags = nlags)
-  #' image(x = v, y = v, z = cov$Lag0, main = "Lag 0")
-  #' image(x = v, y = v, z = cov$Lag1, main = "Lag 1")
-  #' image(x = v, y = v, z = cov$Lag2, main = "Lag 2")
-  #' # Example 3
-  #'
-  #' require(fields)
-  #' N <- 500
-  #' v <- seq(from = 0, to = 1, length.out = 50)
-  #' sig <- 2
-  #' Y <- simulate_iid_brownian_bridge(N, v, sig)
-  #' nlags <- 3
-  #' Y1 <- Y[1:N-1,]
-  #' Y2 <- Y[2:N,]
-  #' cov <- obtain_covariance(Y = Y1,X = Y2, nlags = nlags)
-  #' z_lims <- range(cov$Lag0)
-  #' colors <- heat.colors(12)
-  #' opar <- par(no.readonly = TRUE)
-  #' par(mfrow = c(1,5))
-  #' par(oma=c( 0,0,0,6)) 
-  #' for(k in 0:nlags){
-  #'    image(x=v,
-  #'          y=v,
-  #'          z = cov[[paste0("Lag",k)]],
-  #'          main = paste("Lag",k),
-  #'          col = colors,
-  #'          xlab = "u",
-  #'          ylab = "v")
-  #' }
-  #' par(oma=c( 0,0,0,2.5)) # reset margin to be much smaller.
-  #' image.plot( legend.only=TRUE, legend.width = 2,zlim=z_lims, col = colors)
-  #' par(opar)
-  #' 
-  #' # Perspective Plots
-  #' opar <- par(no.readonly = TRUE)
-  #' par(mfrow = c(1,4))
-  #' for(k in 0:nlags){
-  #'   persp(x=v,
-  #'         y=v,
-  #'         z = cov[[paste0("Lag",k)]],
-  #'         main = paste("Lag",k),
-  #'         xlab = "u",
-  #'         ylab = "v",
-  #'         theta = 330,
-  #'         phi = 30,
-  #'         ticktype = "detailed",
-  #'         zlab = "")
-  #' }
-  #' par(opar)
-  #' }
-  #' @export obtain_covariance
-  
-  nt <- nrow(Y)
-  nu <- ncol(Y)
-  nv <- ncol(X)
-  
-  # Obtain mean function of Y
-  mean.Y <- colMeans(Y)
-  # Obtain mean function of X
-  mean.X <- colMeans(X)
-  
-  # Compute covariance function
-  fun.covariance <- list()
-  for(kk in 0:nlags){
-    fun.covariance[[paste("Lag",kk,sep = "")]] <- matrix(0,nrow = nu,ncol = nv)
-    for(ind_curve in (1+kk):nt){
-      fun.covariance[[paste("Lag",kk,sep = "")]] <- fun.covariance[[paste("Lag",kk,sep = "")]] + (as.numeric(Y[ind_curve,] - mean.Y)) %*% t(as.numeric(X[ind_curve - kk,] - mean.X))
-    }
-    fun.covariance[[paste("Lag",kk,sep = "")]] <- fun.covariance[[paste("Lag",kk,sep = "")]]/(nt-1)
-  }
-  return(fun.covariance)
-}
+
---
+++ R/misc.R
@@ -0,0 +1,322 @@
+mat2fd <- function(mat_obj, range_val = c(0,1),argvals = NULL){
+  #' Obtain a fd object from a matrix
+  #'
+  #' @description This function returns a \code{fd} object
+  #' obtained from the discretized functional observations
+  #' contained in \code{mat_obj}.
+  #'
+  #' It is assumed that the functional observations
+  #' contained in \code{mat_obj} are real observations,
+  #' hence a poligonal base will be used to obtain
+  #' the functional object.
+  #'
+  #' @param mat_obj A matrix that contains the
+  #' discretized functional observations.
+  #' @param range_val A numeric vector of length 2
+  #' that contains the range of the observed functional
+  #' data. By default \code{range_val = c(0,1)}.
+  #' @param argvals Optinal argument that contains
+  #' a strictly increasing vector
+  #' of argument values at which line segments join
+  #' to form a polygonal line. If \code{argvals = NULL},
+  #' it is assumed a equidistant discretization vector.
+  #' By default \code{argvals = NULL}.
+  #' @return A fd object obtained from the functional
+  #' observations in \code{mat_obj}.
+  #'
+  #' @examples
+  #' # Example 1
+  #' N <- 100
+  #' dv <- 30
+  #' v <- seq(from = 0, to = 1, length.out = dv)
+  #' set.seed(150) # For replication
+  #' mat_func_obs <- matrix(rnorm(N*dv), 
+  #'                        nrow = N, 
+  #'                        ncol = dv)
+  #' fd_func_obs <- mat2fd(mat_obj = mat_func_obs,
+  #'                       range_val = range(v),
+  #'                       argvals = v)
+  #' plot(fd_func_obs)
+  #' @export mat2fd
+  
+  # Create functional basis
+  num.discr <- ncol(mat_obj)
+  if (is.null(argvals)){
+    arg.vals  <- seq(from = range_val[1],
+                     to = range_val[2],
+                     length.out = num.discr)
+  }else{
+    arg.vals <- argvals
+  }
+  
+  basis.obj <- fda::create.polygonal.basis(rangeval = range_val,
+                                           argvals  = arg.vals)
+  
+  # Obtain fd object
+  fd.obj <- fda::Data2fd(y = t(mat_obj),
+                         basisobj = basis.obj,
+                         argvals  = arg.vals)
+  
+  return(fd.obj)
+}
+
+
+reconstruct_fd_from_PCA <- function(pca_struct,scores,centerfns = T){
+  #' Obtain the reconstructed curves after PCA
+  #'
+  #' @description This function reconstructs the
+  #' functional curves from a score vector
+  #' using the basis obtained after applying
+  #' functional PCA. This allows the user to
+  #' draw estimations from the joint density of
+  #' the FPCA scores and reconstruct the curves
+  #' for those new scores.
+  #'
+  #' @param pca_struct List obtained after calling
+  #' function \code{pca.fd}.
+  #' @param score Numerical vector that contains the
+  #' scores of the fPCA decomposition for one
+  #' functional observation.
+  #' @param centerfns Logical value specifying
+  #' wheter the FPCA performed used \code{centerfns = T}
+  #' or \code{centerfns = F}. By default
+  #' \code{centerfns = T}.
+  #'
+  #' @return Returns a object of type \code{fd}
+  #' that contains the reconstructed curve.
+  #'
+  #' @examples
+  #' # Example 1
+  #'
+  #' # Simulate fd
+  #' nobs <- 200
+  #' dv <- 10
+  #' basis<-fda::create.bspline.basis(rangeval=c(0,1),nbasis=10)
+  #' set.seed(5)
+  #' C <- matrix(rnorm(nobs*dv), ncol =  dv, nrow = nobs)
+  #' fd_sim <- fda::fd(coef=t(C),basis)
+  #'
+  #' # Perform FPCA
+  #' pca_struct <- fda::pca.fd(fd_sim,nharm = 6)
+  #'
+  #' # Reconstruct first curve
+  #' fd_rec <- reconstruct_fd_from_PCA(pca_struct = pca_struct, scores = pca_struct$scores[1,])
+  #' plot(fd_sim[1])
+  #' plot(fd_rec, add = T, col = "red")
+  #' legend("topright",
+  #'        legend = c("Real Curve", "PCA Reconstructed"),
+  #'        col = c("black","red"),
+  #'        lty = 1)
+  #'
+  #' # Example 2 (Perfect reconstruction)
+  #'
+  #' # Simulate fd
+  #' nobs <- 200
+  #' dv <- 7
+  #' basis<-fda::create.bspline.basis(rangeval=c(0,1),nbasis=dv)
+  #' set.seed(5)
+  #' C <- matrix(rnorm(nobs*dv), ncol =  dv, nrow = nobs)
+  #' fd_sim <- fda::fd(coef=t(C),basis)
+  #'
+  #' # Perform FPCA
+  #' pca_struct <- fda::pca.fd(fd_sim,nharm = dv)
+  #'
+  #' # Reconstruct first curve
+  #' fd_rec <- reconstruct_fd_from_PCA(pca_struct = pca_struct, scores = pca_struct$scores[1,])
+  #' plot(fd_sim[1])
+  #' plot(fd_rec, add = T, col = "red")
+  #' legend("topright",
+  #'        legend = c("Real Curve", "PCA Reconstructed"),
+  #'        col = c("black","red"),
+  #'        lty = 1)
+  #' @export reconstruct_fd_from_PCA
+  
+  if (centerfns){
+    out_curve <- pca_struct$meanfd
+    for(rr in 1:ncol(pca_struct$scores)){
+      out_curve <- out_curve + pca_struct$harmonics[rr]*scores[rr]
+    }
+    return(out_curve)
+  }else{
+    out_curve <- pca_struct$harmonics[1]*scores[1]
+    for(rr in 2:ncol(pca_struct$scores)){
+      out_curve <- out_curve + pca_struct$harmonics[rr]*scores[rr]
+    }
+    return(out_curve)
+  }
+  
+}
+
+integral_operator <- function(operator_kernel, curve, v){
+  #' Integral transformation of a curve using an integral operator
+  #'
+  #' @description Compute the integral transform of the
+  #' curve \eqn{Y_i} with respect to a given integral
+  #' operator \eqn{\Psi}. The transformation is given by
+  #' \deqn{\Psi(Y_{i})(v) = \int \psi(u,v)Y_{i}(u)du}
+  #'
+  #' @param operator_kernel Matrix with the values
+  #' of the kernel surface of the integral operator.
+  #' The dimension of the matrix is \eqn{(g x m)}, where
+  #' \eqn{g} is the number of discretization points of the
+  #' input curve and \eqn{m} is the number of discretization
+  #' points of the output curve.
+  #' @param curve Vector containing the discretized values
+  #' of a functional observation. The dimension of the
+  #' matrix is \eqn{(1 x m)}, where \eqn{m} is the
+  #' number of points observed in the curve.
+  #' @param v Numerical vector specifying the
+  #' discretization points of the curves.
+  #' @return Returns a matrix the same size as
+  #' \code{curve} with the transformed values.
+  #' @examples
+  #' # Example 1
+  #'
+  #' v <- seq(from = 0, to = 1, length.out = 20)
+  #' set.seed(10)
+  #' curve <- sin(v) + rnorm(length(v))
+  #' operator_kernel <- 0.6*(v %*% t(v))
+  #' hat_curve <- integral_operator(operator_kernel,curve,v)
+  #'
+  #' @export integral_operator
+  
+  # Initialize output
+  yhat <- rep(0, times = ncol(operator_kernel))
+  
+  # Perform the integral transformation
+  for(ind_v in 1:ncol(operator_kernel)){
+    yhat[ind_v] <- pracma::trapz(y = operator_kernel[,ind_v]*curve,
+                                 x = v)
+  }
+  return(yhat)
+}
+
+
+fit_ARHp_FPCA <- function(y, v, p, n_harm, show_varprop = T){
+  #' Fit an ARH(p) to a given functional time series
+  #' 
+  #' @description Fit an \eqn{ARH(p)} model to a given functional
+  #' time series. The fitted model is based on the model proposed
+  #' in (Aue et al, 2015), first decomposing the original
+  #' functional observations into a vector time series of \code{n_harm}
+  #' FPCA scores, and then fitting a vector autoregressive
+  #' model of order \eqn{p} (\eqn{VAR(p)}) to the time series
+  #' of the scores. Once fitted, the Karhunen-Loève expansion
+  #' is used to re-transform the fitted values into functional
+  #' observations.
+  #' 
+  #' @param y A matrix containing
+  #' 
+  #' @examples 
+  #' # Example 1
+  #' 
+  #' # Simulate an ARH(1) process
+  #' N <- 250
+  #' dv <- 20
+  #' v <- seq(from = 0, to = 1, length.out = 20)
+  #' 
+  #' phi <- 1.3 * ((v) %*% t(v))
+  #' 
+  #' persp(v,v,phi, 
+  #'       ticktype = "detailed", 
+  #'       main = "Integral operator")
+  #' 
+  #' set.seed(3)
+  #' white_noise <-  simulate_iid_brownian_bridge(N, v = v)
+  #' 
+  #' y <- matrix(nrow = N, ncol = dv)
+  #' y[1,] <- white_noise[1,]
+  #' for(jj in 2:N){
+  #'     y[jj,] <- white_noise[jj,];
+  #'     
+  #'     y[jj,]=y[jj,]+integral_operator(operator_kernel = phi,
+  #'                                     v = v,
+  #'                                     curve = y[jj-1,])
+  #' }
+  #' 
+  #' # Fit an ARH(1) model
+  #' mod <- fit_ARHp_FPCA(y = y,
+  #'                      v = v,
+  #'                      p = 1,
+  #'                      n_harm = 5)
+  #' 
+  #' # Plot results
+  #' plot(v, y[50,], type = "l", lty = 1, ylab = "")
+  #' lines(v, mod$y_est[50,], col = "red")
+  #' legend("bottomleft", legend = c("real","est"),
+  #'        lty = 1, col = c(1,2))
+  #' 
+  #' @references Aue, A., Norinho, D. D., Hörmann, S. (2015).
+  #' \emph{On the Prediction of Stationary Functional 
+  #' Time Series}
+  #' Journal of the American Statistical Association, 
+  #' 110, 378--392. \url{http://www.tandfonline.com/doi/full/10.1080/01621459.2014.909317}
+  #' @export fit_ARHp_FPCA
+  dt <- nrow(y)
+  dv <- length(v)
+  
+  # Step 1: FPCA decomposition of the curves
+  y_fd <- mat2fd(mat_obj = y,argvals = v, range_val = range(v))
+  pca <- fda::pca.fd(fdobj = y_fd, nharm = n_harm)
+  
+  if(show_varprop){
+    plot(1:n_harm,cumsum(pca$varprop),
+         type = "b",
+         pch = 20,
+         main = "% Variance explained by FPCA",
+         xlab = "nº of components",
+         ylab = "% Var. Expl")
+  }
+  
+  y_scores <- pca$scores
+  
+  
+  # Step 2: Fit a VAR(p) model to the scores series
+  y_scores_aux <- as.data.frame(y_scores)
+  names(y_scores_aux) <- paste0("y",1:ncol(y_scores))
+  mod <- vars::VAR(y_scores_aux,p = p)
+  if(show_varprop){
+    summary(mod)
+  }
+  
+  fitted_vals <- fitted(mod)
+  
+  # Fill with NA
+  fitted_vals <- rbind(matrix(NA,nrow = nrow(y_scores)-nrow(fitted_vals),
+                              ncol = ncol(y_scores)),
+                       fitted_vals)
+  
+  if(F){
+    plot(y_scores[,1], type = "l")
+    lines(fitted_vals[,1], col = "red")
+  }
+  
+  
+  # Step 3: reconstruct the curves
+  y_rec <- matrix(nrow = dt, ncol = dv)
+  for(ii in 1:nrow(y)){
+    # fd_aux <- reconstruct_fd_from_PCA(pca_struct = pca,scores = y_scores[ii,],centerfns = T)
+    fd_aux <- reconstruct_fd_from_PCA(pca_struct = pca,scores = fitted_vals[ii,],centerfns = T)
+    
+    fd_aux_mat <- fda::eval.fd(evalarg = v, fdobj = fd_aux)
+    
+    if(F){
+      plot(v,y[ii,], type = "b")
+      lines(v,fd_aux_mat,col = "red")
+    }
+    
+    y_rec[ii,] <- fd_aux_mat
+    
+  }
+  
+  
+  # Prepare output
+  output <- list(y_est = y_rec,
+                 mod = mod,
+                 fpca = pca,
+                 fitted_vals = fitted_vals,
+                 y = y,
+                 v = v)
+  
+  return(output)
+}
